b96fb296c477a9b9b1e81e3e52b9a7a0d152304d
sam-eng <sme388@nyu.edu>
2019-10-18 14:49:18 -0400

    add request to api

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 
 class MoviesViewController: UIViewController {
 
+    // MARK: - Properties
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        retrieveInfo()
     }
     
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Priavte Function
+    func retrieveInfo() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                // TODO: Get the array of movies
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            }
+        }
+        task.resume()
     }
-    */
-
+    
+    
 }
